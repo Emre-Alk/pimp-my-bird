3f54360f721e10146278a172feba70b85295ad29
Emre ALKAN <alkan.emre@hotmail.fr>
2024-02-27 14:14:28 +0100

    feature form new booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,20 +7,20 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @current_user_id = current_user.id
+    @current_user = current_user
     @booking = Booking.new(bookingSet_params)
-    @bird = Bird.find(:bird_id)
+    @bird = Bird.find(params[:bird_id])
     @booking.bird = @bird
-    @booking.user = @current_user_id
-
+    @booking.user = @current_user
     if @booking.save
-      redirect_to user_bookings_path(@current_user_id)
+      redirect_to  user_bookings_path(@current_user)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def index
+    @bookings = Booking.all
   end
 
   private
