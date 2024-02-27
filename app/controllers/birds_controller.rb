ab7acd59f2dd91bee1ca5f0bfc0404f0cc15474c
Bastien RAKOTOARINIVO <bastien.rakotoarinivo@gmail.com>
2024-02-27 14:47:02 +0100

    forms new

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,13 +1,40 @@
 class BirdsController < ApplicationController
-  def index
-  end
+  before_action :set_bird, only: [:show, :update, :destroy]
 
-  def show
+  def index
+    @bird = Bird.all
   end
 
   def new
+    @bird = Bird.new
   end
 
   def create
+    @bird = Bird.new(bird_params)
+    if @bird.save
+      redirect_to bird_path(@bird)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @bird.update(bird_params)
+    redirect_to bird_path(@bird)
+  end
+
+  def destroy
+    @bird.destroy
+    redirect_to birds_path, status: :see_other
+  end
+
+private
+
+  def set_bird
+    @bird = Bird.find(params[:id])
+  end
+
+  def bird_params
+    params.require(:bird).permit(:name, :common_family, :geography, :chant_url)
   end
 end
