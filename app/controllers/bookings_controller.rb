62391d57ebfff97e82cd96f97f3ec1671d503ab0
Emre ALKAN <alkan.emre@hotmail.fr>
2024-02-27 12:27:26 +0100

    form new booking avec def new et def create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,31 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @booking = Booking.new
+    @bird = Bird.find(params[:bird_id])
   end
 
   def create
+    @current_user_id = current_user.id
+    @booking = Booking.new(bookingSet_params)
+    @bird = Bird.find(:bird_id)
+    @booking.bird = @bird
+    @booking.user = @current_user_id
+
+    if @booking.save
+      redirect_to user_bookings_path(@current_user_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+  end
+
+  private
+
+  def bookingSet_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
