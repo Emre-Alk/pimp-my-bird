e6ba3b0b4ff11476eec1954b32f2933eb88c876e
Robin Couret <couret.robin@protonmail.com>
2024-02-29 17:13:26 +0100

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,27 +16,27 @@ Booking.destroy_all
 User.destroy_all
 
 address = [
-  '20 Rue Bellecordière, 69002 Lyon',
-  '20 Pl. de la Croix-Rousse, 69004 Lyon',
-  '28 Cr Franklin Roosevelt, 69006 Lyon',
-  '13 Rue du Général Plessier, 69002 Lyon',
-  '32 Rue Neuve, 69002 Lyon',
-  '231 Rue Paul Bert, 69003 Lyon',
-  '38 Cr Franklin Roosevelt, 69006 Lyon',
-  '14 Rue Passet, 69007 Lyon',
-  '56 Cr Gambetta, 69007 Lyon',
-  '20 Quai Jules Courmont, 69002 Lyon',
-  '20 Rue de Condé, 69002 Lyon',
-  '20 Quai Jules Courmont, 69002 Lyon',
-  '20 Rue Palais Grillet, 69002 Lyon',
-  '20 Quai du Dr Gailleton, 69002 Lyon',
-  '20 Quai de Bondy, 69005 Lyon',
-  '20 Rue Saint-Jean, 69005 Lyon',
-  '20 Rue Bellecordière, 69002 Lyon',
-  '20 Rue Mulet, 69001 Lyon',
-  '44 Rue Sergent Blandan, 69001 Lyon',
-  '20 Rue de la Charité, 69002 Lyon',
-  '20 Rue du Bœuf, 69005 Lyon'
+  '20 Bellecordière, 69002 Lyon',
+  '20 Croix-Rousse, 69004 Lyon',
+  '28 Franklin Roosevelt, 69006 Lyon',
+  '13 Général Plessier, 69002 Lyon',
+  '32 Neuve, 69002 Lyon',
+  '231 Paul Bert, 69003 Lyon',
+  '38 Franklin Roosevelt, 69006 Lyon',
+  '14 Passet, 69007 Lyon',
+  '56 Gambetta, 69007 Lyon',
+  '20 Jules Courmont, 69002 Lyon',
+  '20 Condé, 69002 Lyon',
+  '20 Jules Courmont, 69002 Lyon',
+  '20 Palais Grillet, 69002 Lyon',
+  '20 Gailleton, 69002 Lyon',
+  '20 Bondy, 69005 Lyon',
+  '20 Saint-Jean, 69005 Lyon',
+  '20 Bellecordière, 69002 Lyon',
+  '20 Mulet, 69001 Lyon',
+  '44 Sergent Blandan, 69001 Lyon',
+  '20 Charité, 69002 Lyon',
+  '20 Bœuf, 69005 Lyon'
 ]
 
 puts "Destroy all birds"
